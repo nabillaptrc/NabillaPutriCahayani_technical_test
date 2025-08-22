8b0153b7497d491857ce6321a1c40d367b10c963
nabillaptrc <115095112+nabillaptrc@users.noreply.github.com>
2025-08-22 10:39:49 +0700

    Create 02_clean_tables.sql

---
+++ sql_scripts/02_clean_tables.sql
@@ -0,0 +1,16 @@
+-- Langkah 1: Membersihkan kolom finansial di tabel users
+-- Mengubah kolom dari TEXT menjadi NUMERIC setelah menghapus simbol '$'
+ALTER TABLE users
+    ALTER COLUMN per_capita_income TYPE NUMERIC USING CAST(REPLACE(per_capita_income, '$', '') AS NUMERIC),
+    ALTER COLUMN yearly_income TYPE NUMERIC USING CAST(REPLACE(yearly_income, '$', '') AS NUMERIC),
+    ALTER COLUMN total_debt TYPE NUMERIC USING CAST(REPLACE(total_debt, '$', '') AS NUMERIC);
+
+-- Langkah 2: Membersihkan kolom finansial di tabel cards
+```ALTER TABLE cards
+    ALTER COLUMN credit_limit TYPE NUMERIC USING CAST(REPLACE(credit_limit, '$', '') AS NUMERIC);```
+
+-- Langkah 3: Membersihkan dan mengubah tipe data di tabel transactions
+-- Mengubah kolom amount menjadi NUMERIC dan kolom date menjadi TIMESTAMP
+```ALTER TABLE transactions
+    ALTER COLUMN amount TYPE NUMERIC USING CAST(REPLACE(amount, '$', '') AS NUMERIC),
+    ALTER COLUMN date TYPE TIMESTAMP USING TO_TIMESTAMP(date, 'YYYY-MM-DD HH24:MI:SS');```
